7ca87eb8d5b202619ba8f90cf4011afe947ddade
Khalimon Aleks <alex@Khalimons-MacBook-Pro.local>
2019-06-20 18:29:27 +0300

    Initial commit

---
+++ dist/precache-manifest.7514c70bcaaa5f10022c5057a2327cd0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "00a12876a82effb649bf",
+    "url": "/js/chunk-vendors.097c0760.js"
+  },
+  {
+    "revision": "c6829af54983f423b98e",
+    "url": "/js/app.34ebe01b.js"
+  },
+  {
+    "revision": "d7107f47116862493b56",
+    "url": "/js/about.61418de8.js"
+  },
+  {
+    "revision": "b9d963f78cd53b410b23a2519576136a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b80f2a4d6b2dc0e9eabfcc685c492dc7",
+    "url": "/img/space2.b80f2a4d.png"
+  },
+  {
+    "revision": "c6829af54983f423b98e",
+    "url": "/css/app.08f0a2e7.css"
+  },
+  {
+    "revision": "d7107f47116862493b56",
+    "url": "/css/about.23e8ff4a.css"
+  },
+  {
+    "revision": "6a02faf7ea2a9584134ffe15779a0e44",
+    "url": "/_redirects"
+  }
+];
---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-view/>
+    </div>    
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: white;
+  background-image: url('./assets/space2.png');
+  
+  min-width: 400px;
+  min-height: 700px;
+  
+}
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+
+a {
+  font-weight: bold;
+  color: #3c97ff;
+
+  &:hover {
+    color: #f7f7f7;;
+    font-size: 1.2em;
+    text-decoration: none;
+  }
+}
+</style>
